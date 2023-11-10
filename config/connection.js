91a67e8f7e7a45d33af041b01a497711f30e2b0d
Ayushack101 <ayushtyagi772@gmail.com>
2023-11-10 23:11:26 +0530

    my-change

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,15 @@
-const mongoose = require('mongoose');
-const connectDB = async ()=>{
-try {
-    await mongoose.connect(process.env.MONGO_URL);
+const mongoose = require("mongoose");
+const connectDB = async () => {
+  try {
+    // await mongoose.connect(process.env.MONGO_URL);
+    // await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(
+      "mongodb+srv://admin-ayush:<password>@cluster0.9k5kkc9.mongodb.net/HarpreetDB"
+    );
+
     console.log(`MongoDB connected : ${process.env.MONGO_URL}`);
-} catch (error) {
-    console.log(`mongoose connection with bckend error are : `,error);
-}
-} 
-module.exports = connectDB
+  } catch (error) {
+    console.log(`mongoose connection with bckend error are : `, error);
+  }
+};
+module.exports = connectDB;
